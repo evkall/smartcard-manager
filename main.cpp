8f64b6423291a69bcaeafe6a97ba1591431bd650
evkall <evkall018@gmail.com>
2021-07-08 23:22:00 +0300

    feat: Added sample

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
+#include <windows.h>
+#include <fmt/core.h>
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+
+
 int main(int argc, char** argv) {
+    CLI::App app{ "Smartcard manager" };
+    
+    std::string get_readers;
+    app.add_option("-gr,--get_readers", get_readers, "Get readers list");
+    CLI11_PARSE(app, argc, argv);
+
+    fmt::print("Start");
+
+    SCARDCONTEXT hSC;
+    LONG lReturn;
+
+    lReturn = SCardEstablishContext(SCARD_SCOPE_USER,
+                                    NULL,
+                                    NULL,
+                                    &hSC);
+    if ( lReturn != SCARD_S_SUCCESS)
+        fmt::print("Failed SCardEstablishContext\n");
+    else
+    {
+        fmt::print("Success SCardEstablishContext\n");
+    }
+
+    fmt::print("Stop");
+
     return 0;
 }
- 
